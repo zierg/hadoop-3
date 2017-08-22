9b67fd1751e5bbda33ae369891de4d80eef6c792
zierg <zierg@yandex.ru>
2017-08-22 12:15:12 +0400

    Initial commit

---
+++ src/main/java/homework/hadoop/task3/AdvertisementDriver.java
@@ -0,0 +1,16 @@
+package homework.hadoop.task3;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AdvertisementDriver {
+
+    public static void main(String[] args) {
+
+    }
+
+    static Logger log = LoggerFactory.getLogger(AdvertisementDriver.class);
+}
